302218e2bfc41a032743889a836c9b9cdd37636b
Rahul Adlakha <rahuladlakha26@gmail.com>
2022-05-01 18:11:55 +0530

    Added the motion gesture detector functionality. The app now uses accelerometer sensor to detect thrice shaking gesture when device is shaken thrice along y axis only.

--- app/src/main/java/com/rahul/womensafetyapp/AlertService.kt
+++ app/src/main/java/com/rahul/womensafetyapp/AlertService.kt
@@ -4,12 +4,67 @@ import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Handler
 import android.os.IBinder
 import android.telephony.SmsManager
+import android.util.Log
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
+import java.util.*
 
 class AlertService : Service() {
+
+    var last_acc = 10f
+    private var shake = 0
+    private var lastShakeTime = 0L
+
+    private fun detectThriceShake(){
+        // Toast.makeText(this, "Shake was detected", Toast.LENGTH_SHORT).show()
+        if (shake >= 17){
+            Toast.makeText(this, "Shake was detected" + shake, Toast.LENGTH_SHORT).show()
+            shake = 0
+        }
+        if (Date().time - lastShakeTime < 700 || lastShakeTime == 0L){
+            shake++
+            lastShakeTime = Date().time
+            //Toast.makeText(this, "" + shake, Toast.LENGTH_SHORT).show()
+            Log.i("Shake ", "" +shake)
+        }
+        else {
+            lastShakeTime = 0
+            shake = 0
+        }
+    }
+
+    private fun activateSensors(){
+        val sensorManager: SensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        val sensorShake = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(object : SensorEventListener {
+            override fun onSensorChanged(p0: SensorEvent?) {
+                if (p0 == null) return
+                var x = p0.values[0]
+                var y = p0.values[1]
+                var z = p0.values[2]
+                if (y - last_acc  > 4 || y - last_acc < -4) {
+                    this@AlertService.detectThriceShake()
+                    Log.i("acc", "" + (y - last_acc))
+                }
+
+//                    Toast.makeText(this@MainActivity, "Shake event detected", Toast.LENGTH_SHORT).show()
+                last_acc = y
+
+            }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+                return
+            }
+        }, sensorShake, SensorManager.SENSOR_DELAY_NORMAL )
+
+    }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val pendingIntent: PendingIntent =
             Intent(this, AlertService::class.java).let { notificationIntent ->
@@ -24,13 +79,18 @@ class AlertService : Service() {
             .setTicker("tickerText")
             .build()
 
+        activateSensors()
+
 // Notification ID cannot be 0.
         startForeground(98, notification)
 
-        Handler().postDelayed(Runnable {
-            val smsManager: SmsManager = SmsManager.getDefault()
-            smsManager.sendTextMessage("+918570962219", null, "sms message", null, null)
-        }, 5000)
+//        Handler().postDelayed(Runnable {
+////            val smsManager: SmsManager = SmsManager.getDefault()
+////            smsManager.sendTextMessage("+918570962219", null, "sms message sent from Women safety app", null, null)
+//            this@AlertService.stopForeground(true)
+//            stopSelf() //stopForeground stops the service but it is still running in background.
+////          //  stopSelf stops the service itself
+//        }, 5000)
         return super.onStartCommand(intent, flags, startId)
 
     }
--- app/src/main/java/com/rahul/womensafetyapp/MainActivity.kt
+++ app/src/main/java/com/rahul/womensafetyapp/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.rahul.womensafetyapp
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val permissions = arrayOf( Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_BACKGROUND_LOCATION, Manifest.permission.SEND_SMS, Manifest.permission.FOREGROUND_SERVICE)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPermissions()
         createNotificationChannel()
 
         val intent = Intent(this, AlertService::class.java) // Build the intent for the service
         applicationContext.startForegroundService(intent)
+
+
     }
 
     private fun createNotificationChannel() {
@@ -32,4 +48,27 @@ class MainActivity : AppCompatActivity() {
             notificationManager.createNotificationChannel(channel)
         }
     }
+
+    private fun checkPermissions( ){
+        for ( i in 0..(permissions.size-1)){
+           if (ContextCompat.checkSelfPermission(this, permissions[i]) != PackageManager.PERMISSION_GRANTED) {
+             //  Toast.makeText(this, permissions[i], Toast.LENGTH_SHORT).show()
+               ActivityCompat.requestPermissions(this, arrayOf(permissions[i]), i)
+           }
+       }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //if ( requestCode != permissions.size-1) {
+           // Toast.makeText(this, permissions[requestCode], Toast.LENGTH_SHORT).show()
+           //checkPermissions()
+       // }
+    }
 }
+
+
