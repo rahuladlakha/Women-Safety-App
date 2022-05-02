d383beaad215a3ce38f47c475f181ea380dd308c
Rahul Adlakha <rahuladlakha26@gmail.com>
2022-05-02 21:12:30 +0530

    The app is now accessing location in background and sends it to the contact through sms. A link for google maps is also sent in the sms which directly opens the current location of the user. Though the app has started to become a little unresponsive because of this much work so will try to make it more responsive later on.

--- app/src/main/java/com/rahul/womensafetyapp/AlertService.kt
+++ app/src/main/java/com/rahul/womensafetyapp/AlertService.kt
@@ -1,9 +1,11 @@
 package com.rahul.womensafetyapp
 
+import android.Manifest
 import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -16,7 +18,12 @@ import android.os.IBinder
 import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationToken
 import java.util.*
 
 class AlertService : Service() {
@@ -25,6 +32,7 @@ class AlertService : Service() {
     private var shake = 0
     private var lastShakeTime = 0L
     lateinit var notificationTone : Ringtone
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
 
     private fun detectThriceShake(){
         // Toast.makeText(this, "Shake was detected", Toast.LENGTH_SHORT).show()
@@ -32,6 +40,28 @@ class AlertService : Service() {
             Toast.makeText(this, "Shake was detected" + shake, Toast.LENGTH_SHORT).show()
             notificationTone.play()
             shake = 0
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            fusedLocationClient.lastLocation.addOnSuccessListener{
+                Log.i("location", "${it?.longitude}, ${it?.latitude}")
+                val smsManager: SmsManager = SmsManager.getDefault()
+                smsManager.sendTextMessage("+918570962219", null, "sms message sent from Women safety app: http://maps.google.com/maps?q=loc:${it?.latitude},${it?.longitude}", null, null)
+            }
         }
         if (Date().time - lastShakeTime < 700 || lastShakeTime == 0L){
             shake++
@@ -67,7 +97,7 @@ class AlertService : Service() {
             override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
                 return
             }
-        }, sensorShake, SensorManager.SENSOR_DELAY_NORMAL )
+        }, sensorShake, SensorManager.SENSOR_DELAY_FASTEST )
 
     }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -86,8 +116,10 @@ class AlertService : Service() {
 
         val notif = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         notificationTone = RingtoneManager.getRingtone(this, notif)
-
         activateSensors()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this) // For accessing location in the
+        //background we use the FusedLocation api
+
 
 // Notification ID cannot be 0.
         startForeground(98, notification)
