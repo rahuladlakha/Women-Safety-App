d0ad2b9b86c26cfb536395a71e673e824ab97ca3
Rahul Adlakha <rahuladlakha26@gmail.com>
2022-06-21 21:56:34 +0530

    Made some UI improvements and added functionality in the SignUpActivity. The SignUpActivity now proceeds to MainActivity when user provides name and atleast one emergency contact.

--- app/src/main/java/com/rahul/womensafetyapp/SignUpActivity.kt
+++ app/src/main/java/com/rahul/womensafetyapp/SignUpActivity.kt
@@ -1,7 +1,11 @@
 package com.rahul.womensafetyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +13,29 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
         supportActionBar?.hide()
 
+
+        saveButton.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                val name = NameEditText.text.toString().trim()
+                val emContacts = arrayOf<String>(emergencyContact1.text.toString().trim(), emergencyContact2.text.toString().trim(), emergencyContact3.text.toString().trim())
+                if (name.isEmpty()) {
+                        Toast.makeText(
+                            this@SignUpActivity,
+                            "Name field can't be empty!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        return
+                } else if (emContacts[0].isEmpty()) {
+                        Toast.makeText(
+                            this@SignUpActivity,
+                            "Please provide atleast one emergency contact !",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        return
+                }
+                startActivity(Intent(this@SignUpActivity, MainActivity::class.java))
+                this@SignUpActivity.finish()
+            }
+        })
     }
 }
