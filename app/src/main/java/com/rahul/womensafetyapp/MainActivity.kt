8a0e4c6caa23ef2d14caf48f58077d7e762c9c83
Rahul Adlakha <rahuladlakha26@gmail.com>
2022-06-24 22:25:59 +0530

    Enabled the functionality to ask users for granting permission required for the app to function.
    User is shown why each permission is required using Alert diologs.

--- app/src/main/java/com/rahul/womensafetyapp/MainActivity.kt
+++ app/src/main/java/com/rahul/womensafetyapp/MainActivity.kt
@@ -23,31 +23,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    val permissions = arrayOf( Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_BACKGROUND_LOCATION, Manifest.permission.SEND_SMS, Manifest.permission.FOREGROUND_SERVICE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
         material_button.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                enableProtection(protectionStatus.text != "ON")
+                enableProtection(this@MainActivity, protectionStatus.text != "ON")
             }
         })
-
-        checkPermissions()
+        checkPermissions(this)
         createNotificationChannel()
-        enableProtection(AlertService.isServiceRunning)
+        enableProtection(this, AlertService.isServiceRunning)
 
     }
-    fun enableProtection(b : Boolean){
-        material_button.backgroundTintList = ColorStateList.valueOf(getColor(if (b) R.color.green else R.color.red))
-        protectionStatus.text = if (b) "ON" else "OFF"
-        if (b) {
-            startForegroundService(Intent(this, AlertService::class.java))
-        } else {
-            stopService(Intent(this, AlertService::class.java))
-        }
-    }
+
 
     private fun createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
@@ -65,26 +57,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkPermissions( ){
-        for ( i in 0..(permissions.size-1)){
-           if (ContextCompat.checkSelfPermission(this, permissions[i]) != PackageManager.PERMISSION_GRANTED) {
-             //  Toast.makeText(this, permissions[i], Toast.LENGTH_SHORT).show()
-               ActivityCompat.requestPermissions(this, arrayOf(permissions[i]), i)
-           }
-       }
-    }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
+        if (!checkIfPermissionsGranted(this))
+            checkPermissions(this)
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        //if ( requestCode != permissions.size-1) {
-           // Toast.makeText(this, permissions[requestCode], Toast.LENGTH_SHORT).show()
-           //checkPermissions()
-       // }
+
     }
+
 }
 
 
---
+++ app/src/main/java/com/rahul/womensafetyapp/Utility.kt
@@ -0,0 +1,72 @@
+package com.rahul.womensafetyapp
+//this file contains a few necessary functions and classes used often in the app
+import android.Manifest
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.res.ColorStateList
+import android.net.Uri
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.nio.channels.InterruptedByTimeoutException
+
+private val permissions = arrayOf( Manifest.permission.ACCESS_FINE_LOCATION ,
+    Manifest.permission.ACCESS_COARSE_LOCATION ,
+    Manifest.permission.ACCESS_BACKGROUND_LOCATION, Manifest.permission.SEND_SMS, Manifest.permission.FOREGROUND_SERVICE
+ )
+private val permissionDes = arrayOf(
+    "Location permission" to "This permission is required to access Locaton service on your device.",
+    "Location permission" to "This permission is required to access Locaton service on your device.",
+    "Background Location permission" to "Allows the app to access this device's location in background. Necessary for the app to function.\n\n Note: If tapping the below button doesn't open the permissions dialog, manually provide permission by going to app permission settings and selecting \"Allow all the time\" for location permisssion",
+    "SMS permission" to "SMS permission is required to send SMS to your selected emergency contacts.",
+    "Foreground Service permission" to "Allows the app to run in background."
+)
+
+fun checkIfPermissionsGranted(activity: AppCompatActivity) : Boolean =
+    permissions.all{ContextCompat.checkSelfPermission(activity, it) == PackageManager.PERMISSION_GRANTED}
+
+fun checkPermissions( activity: AppCompatActivity) {
+
+    fun requestPermission( perIndex : Int){
+        AlertDialog.Builder(activity)
+            .setIcon(R.drawable.ic_info_outline)
+            .setTitle("Please provide the ${permissionDes[perIndex].first}")
+            .setMessage(permissionDes[perIndex].second)
+            .setPositiveButton("Proceed to grant", object : DialogInterface.OnClickListener{
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+                    if (permissions[perIndex] == Manifest.permission.ACCESS_BACKGROUND_LOCATION) {
+                        Toast.makeText(activity, "Select \"Allow all the time\" in Location Permissions", Toast.LENGTH_LONG).show()
+//                         if (activity.shouldShowRequestPermissionRationale(permissions[perIndex]))
+//                             activity.startActivity(
+//                                Intent().setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+//                                    .setData(Uri.fromParts("package", activity.packageName, null))
+//                            )
+//
+                    }
+                    ActivityCompat.requestPermissions(activity, arrayOf(permissions[perIndex]), perIndex)
+                }
+            })
+            .show()
+    }
+
+    val i = permissions.indexOfFirst { ContextCompat.checkSelfPermission(activity, it) != PackageManager.PERMISSION_GRANTED }
+    if (i in 0..(permissions.size-1) )
+        requestPermission(i)
+        // ActivityCompat.requestPermissions(activity, arrayOf(permissions[i]), i)
+}
+
+fun enableProtection(activity: AppCompatActivity,b : Boolean){
+    activity.material_button.backgroundTintList = ColorStateList.valueOf(activity.getColor(if (b) R.color.green else R.color.red))
+    activity.protectionStatus.text = if (b) "ON" else "OFF"
+    if (b) {
+        activity.startForegroundService(Intent(activity, AlertService::class.java))
+    } else {
+        activity.stopService(Intent(activity, AlertService::class.java))
+    }
+}
