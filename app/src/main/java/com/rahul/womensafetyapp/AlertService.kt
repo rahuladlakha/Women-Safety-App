ccf425ef12c4c9b7638f38bb134e66d4172cb809
Rahul Adlakha <rahuladlakha26@gmail.com>
2022-07-30 19:33:58 +0530

    Added separate icon for notification

--- app/src/main/java/com/rahul/womensafetyapp/AlertService.kt
+++ app/src/main/java/com/rahul/womensafetyapp/AlertService.kt
@@ -169,7 +169,7 @@ class AlertService : Service() {
         val notification : Notification = NotificationCompat.Builder(this, "service_notification")
             .setContentTitle("Women Safety App is running")
             .setContentText("Protection is on")
-            .setSmallIcon(R.drawable.ic_women_safety_app)
+            .setSmallIcon(R.drawable.ic_women_safety_app_outline)
             .setContentIntent(pendingIntent)
             .setTicker("tickerText")
             .build()
