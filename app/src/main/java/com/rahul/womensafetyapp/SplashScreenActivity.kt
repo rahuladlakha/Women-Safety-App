61c1cd41cd0a953a2989f8b85374406df12f7623
Rahul Adlakha <rahuladlakha26@gmail.com>
2022-09-30 15:25:26 +0530

    Made a few changes and done final testing with the Chinese langauge. The app can now be updated with the language on playstore.

--- app/src/main/java/com/rahul/womensafetyapp/SplashScreenActivity.kt
+++ app/src/main/java/com/rahul/womensafetyapp/SplashScreenActivity.kt
@@ -4,12 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import java.util.*
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         supportActionBar?.hide()
+
+
+        /*
+        //Simply change the String argument of Locale constructor in first line to change language for app.
+        //The below code is there for testing purposes
+        val locale = Locale("zh")
+        Locale.setDefault(locale)
+        val res = this.resources
+        val config = res.configuration
+        config.locale = locale
+        config.setLayoutDirection(locale)
+        res.updateConfiguration(config, res.displayMetrics)
+         */
+
+
 
         Handler().postDelayed(Runnable{
             val intent = Intent(this@SplashScreenActivity, SignUpActivity::class.java)
